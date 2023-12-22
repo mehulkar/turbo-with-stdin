766a2ea9b0c4d8c36ff6d704963d1a36b80ef7ca
Mehul Kar <mehul.kar@vercel.com>
2023-12-21 20:23:15 -0600

    another playground package

---
+++ packages/playground-pty/index.mjs
@@ -0,0 +1,24 @@
+import * as os from "node:os";
+import * as pty from "node-pty";
+
+const shell = os.platform() === "win32" ? "powershell.exe" : "bash";
+
+const ptyProcess = pty.spawn(shell, [], {
+  name: "xterm-color",
+  cols: 80,
+  rows: 30,
+  cwd: process.cwd(),
+  env: process.env,
+});
+
+ptyProcess.onData((data) => {
+  process.stdout.write(data);
+});
+
+process.stdin.on("data", (data) => {
+  ptyProcess.write(data);
+});
+
+ptyProcess.write("ls\r");
+ptyProcess.resize(100, 40);
+ptyProcess.write("ls\r");
