2a07f04cef0947f3cb032859ba328fdac70b5444
Mehul Kar <mehul.kar@vercel.com>
2024-01-08 16:27:14 -0600

    add tokio bin

--- src/bin/tokio.rs
+++ src/bin/tokio.rs
@@ -2,17 +2,14 @@ use tokio::process::Command;
 
 #[tokio::main]
 async fn main() {
-    let mut child = Command::new("pnpm")
+    let child = Command::new("pnpm")
         .args(vec!["-F", "pkg-a", "run", "build"])
         .stdout(std::process::Stdio::inherit())
         .stdin(std::process::Stdio::inherit())
         .spawn()
         .expect("failed to execute child");
 
-    let ecode = child
-        .wait()
-        .await
-        .expect("failed to wait on child");
+    let ecode = child.await.expect("failed to wait on child");
 
     println!("exited with: {}", ecode);
 }
